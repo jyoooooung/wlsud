d2e9e9a2af6233d9c5171d6f3df9cc604ecfe647
jyoooooung <jyoooooung@naver.com>
2020-09-23 18:27:47 +0900

    2020/09/23

---
+++ Car.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include "Car.h"
+using namespace std;
+
+void Car::InitMembers(const char* ID, int fuel)
+{
+	strcpy(gamerID, ID);
+	fuelGauge = fuel;
+	curSpeed = 0;
+}
+void Car::ShowCarState()
+{
+	cout << "소유자ID : " << gamerID << endl;
+	cout << "연료량 : " << fuelGauge << "%" << endl;
+	cout << "현재속도 : " << curSpeed << "km/s" << endl << endl;
+}
+void Car::Accel()
+{
+	if (fuelGauge <= 0)
+		return;
+	else
+		fuelGauge -= CAR_CONST::FUEL_STEP;
+
+	if ((curSpeed + CAR_CONST::ACC_STEP) >= CAR_CONST::MAX_SPD)
+	{
+		curSpeed = CAR_CONST::MAX_SPD;
+		return;
+	}
+
+	curSpeed += CAR_CONST::ACC_STEP;
+}
+void Car::Break()
+{
+	if (curSpeed < CAR_CONST::BRK_STEP)
+	{
+		curSpeed = 0;
+		return;
+	}
+
+	curSpeed -= CAR_CONST::BRK_STEP;
+}
+
+int main(void)
+{
+	Car run99;
+	run99.InitMembers("run99", 100);
+	run99.Accel();
+	run99.Accel();
+	run99.Accel();
+	run99.ShowCarState();
+	run99.Break();
+	run99.ShowCarState();
+	return 0;
+}
